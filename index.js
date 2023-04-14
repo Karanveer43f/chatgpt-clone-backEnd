11392162768541edb67c69381fdeee8ed6017edd
Karanveer <karanveer43f@gmail.com>
2023-04-14 19:20:35 +0530

    css now works

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("dotenv").config();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./assets"));
 app.listen(port, () => console.log(`Listening on ${port}`));
 
 let questionForGPT;
